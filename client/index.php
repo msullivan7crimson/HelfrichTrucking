debe15c8ec6a45502de23f2144240db568f6a5e5
Matthew Sullivan <msullivan7@crimson.ua.edu>
2024-05-13 20:28:26 -0500

    newest

--- client/index.php
+++ client/index.php
@@ -6,38 +6,38 @@
     <title>My Web Application</title>
 
     <!-- CSS Files -->
-    <link rel="stylesheet" href="client/StylingAssets/assets/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="client/StylingAssets/assets/dist/css/bootstrap.rtl.min.css">
-    <link rel="stylesheet" href="client/StylingAssets/styles/LandingPage.css">
+    <link rel="stylesheet" href="/client/StylingAssets/assets/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/client/StylingAssets/assets/dist/css/bootstrap.rtl.min.css">
+    <link rel="stylesheet" href="/client/styles/LandingPage.css">
 
     <!-- Fonts -->
     <style>
         @font-face {
             font-family: 'Consolas';
-            src: url('Consolas.ttf') format('truetype');
+            src: url('/client/StylingAssets/assets/Consolas.ttf') format('truetype');
         }
     </style>
 </head>
 <body>
     <!-- Navbar -->
-    <?php include_once("fixedNavbar.html"); ?>
+    <?php include_once("client/pages/fixedNavbar.html"); ?>
 
     <!-- Landing Page -->
-    <?php include_once("landingPage.html"); ?>
+    <?php include_once("client/pages/landingPage.html"); ?>
 
     <!-- About Us -->
-    <?php include_once("aboutUs.html"); ?>
+    <?php include_once("client/pages/aboutUs.html"); ?>
 
     <!-- Contact -->
-    <?php include_once("contact.html"); ?>
+    <?php include_once("client/pages/contact.html"); ?>
 
     <!-- Images -->
-    <img src="client/StylingAssets/assets/brand/bootstrap-logo-white.svg" alt="Bootstrap Logo White">
-    <img src="client/StylingAssets/assets/brand/bootstrap-logo.svg" alt="Bootstrap Logo">
+    <img src="/client/StylingAssets/assets/brand/bootstrap-logo-white.svg" alt="Bootstrap Logo White">
+    <img src="/client/StylingAssets/assets/brand/bootstrap-logo.svg" alt="Bootstrap Logo">
 
     <!-- JavaScript Files -->
-    <script src="client/StylingAssets/assets/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="client/StylingAssets/js/color-modes.js"></script>
-    <script src="client/scripts/landingpage.js"></script>
+    <script src="/client/StylingAssets/assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/client/StylingAssets/assets/js/color-modes.js"></script>
+    <script src="/client/scripts/landingpage.js"></script>
 </body>
 </html>
