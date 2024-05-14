5fb62d33c135e0192c57d95c680956cddc7c0b54
Matthew Sullivan <msullivan7@crimson.ua.edu>
2024-05-13 20:40:43 -0500

    current website

--- client/index.php
+++ client/index.php
@@ -6,38 +6,38 @@
     <title>Helfrich Trucking & Excavating</title>
 
     <!-- CSS Files -->
-    <link rel="stylesheet" href="HelfrichTrucking/client/StylingAssets/assets/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="HelfrichTrucking/client/StylingAssets/assets/dist/css/bootstrap.rtl.min.css">
-    <link rel="stylesheet" href="HelfrichTrucking/client/styles/LandingPage.css">
+    <link rel="stylesheet" href="/client/StylingAssets/assets/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/client/StylingAssets/assets/dist/css/bootstrap.rtl.min.css">
+    <link rel="stylesheet" href="/client/styles/LandingPage.css">
 
     <!-- Fonts -->
     <style>
         @font-face {
             font-family: 'Consolas';
-            src: url('HelfrichTrucking/client/StylingAssets/assets/Consolas.ttf') format('truetype');
+            src: url('/client/StylingAssets/assets/Consolas.ttf') format('truetype');
         }
     </style>
 </head>
 <body>
     <!-- Navbar -->
-    <?php include_once("HelfrichTrucking/client/fixedNavbar.html"); ?>
+    <?php include_once("fixedNavbar.html"); ?>
 
     <!-- Landing Page -->
-    <?php include_once("HelfrichTrucking/client/landingPage.html"); ?>
+    <?php include_once("landingPage.html"); ?>
 
     <!-- About Us -->
-    <?php include_once("HelfrichTrucking/client/aboutUs.html"); ?>
+    <?php include_once("aboutUs.html"); ?>
 
     <!-- Contact -->
-    <?php include_once("HelfrichTrucking/client/contact.html"); ?>
+    <?php include_once("contact.html"); ?>
 
     <!-- Images -->
-    <img src="HelfrichTrucking/client/StylingAssets/assets/brand/bootstrap-logo-white.svg" alt="Bootstrap Logo White">
-    <img src="HelfrichTrucking/client/StylingAssets/assets/brand/bootstrap-logo.svg" alt="Bootstrap Logo">
+    <img src="/client/StylingAssets/assets/brand/bootstrap-logo-white.svg" alt="Bootstrap Logo White">
+    <img src="/client/StylingAssets/assets/brand/bootstrap-logo.svg" alt="Bootstrap Logo">
 
     <!-- JavaScript Files -->
-    <script src="HelfrichTrucking/client/StylingAssets/assets/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="HelfrichTrucking/client/StylingAssets/assets/js/color-modes.js"></script>
-    <script src="HelfrichTrucking/client/scripts/landingpage.js"></script>
+    <script src="/client/StylingAssets/assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/client/StylingAssets/js/color-modes.js"></script>
+    <script src="/client/scripts/landingpage.js"></script>
 </body>
 </html>
