42481dfa13595601b3d4728b945a262322701470
Matthew Sullivan <msullivan7@crimson.ua.edu>
2024-05-13 19:56:08 -0500

    new

--- client/index.php
+++ client/index.php
@@ -1,18 +1,45 @@
-<?php include_once("aboutUs.html");?>
-<?php include_once("contact.html");?>
-<?php include_once("fixedNavbar.html");?>
-<?php include_once("landingPage.html");?>
-<?php include_once("bootstrap-logo-white.svg");?>
-<?php include_once("bootstrap-logo.svg");?>
-<?php include_once("bootstrap.bundle.min.js");?>
-<?php include_once("bootstrap.bundle.min.js.map");?>
-<?php include_once("bootstrap.min.css");?>
-<?php include_once("bootstrap.min.css.map");?>
-<?php include_once("bootstrap.rtl.min.css");?>
-<?php include_once("bootstrap.rtl.min.css.map");?>
-<?php include_once("color-modes.js");?>
-<?php include_once("Consolas.ttf");?>
-<?php include_once("contact.css");?>
-<?php include_once("contact.js");?>
-<?php include_once("LandingPage.css");?>
-<?php include_once("landingpage.js");?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Web Application</title>
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="client/assets/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="client/assets/dist/css/bootstrap.rtl.min.css">
+    <link rel="stylesheet" href="client/styles/contact.css">
+    <link rel="stylesheet" href="client/styles/LandingPage.css">
+
+    <!-- Fonts -->
+    <style>
+        @font-face {
+            font-family: 'Consolas';
+            src: url('Consolas.ttf') format('truetype');
+        }
+    </style>
+</head>
+<body>
+    <!-- Navbar -->
+    <?php include_once("fixedNavbar.html"); ?>
+
+    <!-- Landing Page -->
+    <?php include_once("landingPage.html"); ?>
+
+    <!-- About Us -->
+    <?php include_once("aboutUs.html"); ?>
+
+    <!-- Contact -->
+    <?php include_once("contact.html"); ?>
+
+    <!-- Images -->
+    <img src="client/assets/brand/bootstrap-logo-white.svg" alt="Bootstrap Logo White">
+    <img src="client/assets/brand/bootstrap-logo.svg" alt="Bootstrap Logo">
+
+    <!-- JavaScript Files -->
+    <script src="client/assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="client/js/color-modes.js"></script>
+    <script src="client/scripts/contact.js"></script>
+    <script src="client/scripts/landingpage.js"></script>
+</body>
+</html>
