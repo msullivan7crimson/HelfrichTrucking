bf0bc764d311d7a30a7f55fedf04fa39f282bf9d
Matthew Sullivan <msullivan7@crimson.ua.edu>
2024-05-13 19:20:41 -0500

    newest

---
+++ client/pages/index.php
@@ -0,0 +1,5 @@
+<?php include_once("/client/pages/aboutUs.html");?>
+<?php include_once("/client/pages/contact.html");?>
+<?php include_once("/client/pages/fixedNavbar.html");?>
+<?php include_once("/client/pages/landingPage.html");?>
+
