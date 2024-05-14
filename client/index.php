af7766a8bde8acaa5f7ff91dc6e1191f23d493b9
Matthew Sullivan <msullivan7@crimson.ua.edu>
2024-05-14 10:16:51 -0500

    new changes

--- client/index.php
+++ client/index.php
@@ -6,15 +6,15 @@
     <title>Helfrich Trucking & Excavating</title>
 
     <!-- CSS Files -->
-    <link rel="stylesheet" href="/client/StylingAssets/assets/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/client/StylingAssets/assets/dist/css/bootstrap.rtl.min.css">
-    <link rel="stylesheet" href="/client/styles/LandingPage.css">
+    <link rel="stylesheet" href="/StylingAssets/assets/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/StylingAssets/assets/dist/css/bootstrap.rtl.min.css">
+    <link rel="stylesheet" href="/styles/LandingPage.css">
 
     <!-- Fonts -->
     <style>
         @font-face {
             font-family: 'Consolas';
-            src: url('/client/StylingAssets/assets/Consolas.ttf') format('truetype');
+            src: url('/StylingAssets/assets/Consolas.ttf') format('truetype');
         }
     </style>
 </head>
@@ -32,12 +32,12 @@
     <?php include_once("contact.html"); ?>
 
     <!-- Images -->
-    <img src="/client/StylingAssets/assets/brand/bootstrap-logo-white.svg" alt="Bootstrap Logo White">
-    <img src="/client/StylingAssets/assets/brand/bootstrap-logo.svg" alt="Bootstrap Logo">
+    <img src="/StylingAssets/assets/brand/bootstrap-logo-white.svg" alt="Bootstrap Logo White">
+    <img src="/StylingAssets/assets/brand/bootstrap-logo.svg" alt="Bootstrap Logo">
 
     <!-- JavaScript Files -->
     <script src="/StylingAssets/assets/dist/js/bootstrap.bundle.min.js"></script>
     <script src="/StylingAssets/js/color-modes.js"></script>
-    <script src="/client/scripts/landingpage.js"></script>
+    <script src="/scripts/landingpage.js"></script>
 </body>
 </html>
