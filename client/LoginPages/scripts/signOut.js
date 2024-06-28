df8b1cd445fbedca3e7ea3862e5101ebd302451e
Matthew Sullivan <msullivan7@crimson.ua.edu>
2024-06-28 10:52:38 -0500

    modifications

--- client/LoginPages/scripts/signOut.js
+++
@@ -1,6 +0,0 @@
-function customerSignOut(event){
-    event.stopPropagation()
-    alert("You have been signed out! Returning to home page.")
-    localStorage.clear()
-    window.location.href='/client/LandingPage/pages/landingPage.html';
-}
